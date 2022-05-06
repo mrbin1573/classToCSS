3d644c359c68b1926c21d500bd6ae3f141d07a71
hebin <1573406407@qq.com>
2022-05-06 14:30:25 +0800

    FEAT：修改单词拼写

--- modules/classMap.js
+++ modules/classMap.js
@@ -7,28 +7,28 @@
  *          - value     class="xx-value"
  *          - percent   value/100
  *          - className class="value"
- *    hasSuffix 是否有单位后缀，对应config.suffix
+ *    hasUnit 是否有单位后缀，对应config.unit
  *    placeholder snippets：enter后聚焦的提示内容
  *    accept 校验
  */
 // TODO accept
 const classMap = new Map([
-  ["m", { styleName: "margin", hasSuffix: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
-  ["mt", { styleName: "margin-top", hasSuffix: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
-  ["mr", { styleName: "margin-right", hasSuffix: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
-  ["mb", { styleName: "margin-bottom", hasSuffix: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
-  ["ml", { styleName: "margin-left", hasSuffix: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
+  ["m", { styleName: "margin", hasUnit: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
+  ["mt", { styleName: "margin-top", hasUnit: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
+  ["mr", { styleName: "margin-right", hasUnit: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
+  ["mb", { styleName: "margin-bottom", hasUnit: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
+  ["ml", { styleName: "margin-left", hasUnit: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
 
-  ["p", { styleName: "padding", hasSuffix: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
-  ["pt", { styleName: "padding-top", hasSuffix: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
-  ["pr", { styleName: "padding-right", hasSuffix: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
-  ["pb", { styleName: "padding-bottom", hasSuffix: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
-  ["pl", { styleName: "padding-left", hasSuffix: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
+  ["p", { styleName: "padding", hasUnit: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
+  ["pt", { styleName: "padding-top", hasUnit: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
+  ["pr", { styleName: "padding-right", hasUnit: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
+  ["pb", { styleName: "padding-bottom", hasUnit: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
+  ["pl", { styleName: "padding-left", hasUnit: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
 
-  ["top", { styleName: "top", hasSuffix: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
-  ["right", { styleName: "right", hasSuffix: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
-  ["bottom", { styleName: "bottom", hasSuffix: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
-  ["left", { styleName: "left", hasSuffix: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
+  ["top", { styleName: "top", hasUnit: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
+  ["right", { styleName: "right", hasUnit: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
+  ["bottom", { styleName: "bottom", hasUnit: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
+  ["left", { styleName: "left", hasUnit: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
 
   ["static", { styleName: "position", valType: "className", placeholder: "", accept: [String] }],
   ["relative", { styleName: "position", valType: "className", placeholder: "", accept: [String] }],
@@ -44,9 +44,9 @@ const classMap = new Map([
   ["justify", { styleName: "justify-content", prefix: "", valType: "classNameValue", placeholder: "|", accept: [String] }],
   ["align", { styleName: "align-items", valType: "classNameValue", placeholder: "|", accept: [String] }],
 
-  ["radius", { styleName: "border-radius", hasSuffix: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
+  ["radius", { styleName: "border-radius", hasUnit: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
 
-  ["fs", { styleName: "font-size", hasSuffix: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
+  ["fs", { styleName: "font-size", hasUnit: true, valType: "classNameValue", placeholder: "Number", accept: [Number] }],
   ["fw", { styleName: "font-weight", valType: "classNameValue", placeholder: "Number|String", accept: [Number, String] }],
   ["fm", { styleName: "font-family", valType: "classNameValue", placeholder: "String", accept: [String] }],
   ["color", { styleName: "color", valType: "classNameValue", placeholder: "String", accept: [String] }],
@@ -54,7 +54,7 @@ const classMap = new Map([
   ["bg", { styleName: "background-color", valType: "classNameValue", placeholder: "String", accept: [String] }],
 
   ["opacity", { styleName: "opacity", valType: "percent", placeholder: "0~100", accept: [Number] }],
-  ["transition", { styleName: "transition", valType: "percent", suffix: "ms", placeholder: "ms", accept: [Number] }],
+  ["transition", { styleName: "transition", valType: "percent", unit: "ms", placeholder: "ms", accept: [Number] }],
 
   ["border-box", { styleName: "box-sizing", valType: "className", placeholder: "", accept: [String] }],
   ["content-box", { styleName: "box-sizing", valType: "className", placeholder: "", accept: [String] }],
--- modules/getStyle.js
+++ modules/getStyle.js
@@ -1,5 +1,5 @@
+const { workspace } = require("vscode")
 const classMap = require("./classMap")
-const { suffix, ratio } = require("./config")
 const { isNumberStr } = require("./utils")
 
 /**
@@ -8,10 +8,10 @@ const { isNumberStr } = require("./utils")
  * @return {String} css
  */
 const getStyle = (classAry) => {
+  const { unit, valueRatio } = workspace.getConfiguration("classtocss")
   return classAry.reduce((allStyle, className) => {
     const params = /(\w+)\-?(.*)/gi.exec(className)
     if (!params) return allStyle
-
     /**
      * className全名匹配
      * 若无再匹配key
@@ -20,9 +20,9 @@ const getStyle = (classAry) => {
     const [, key, classValue] = params
     let mapInfo = classMap.get(className)
     if (!mapInfo) mapInfo = classMap.get(key)
-    if (!mapInfo) return
+    if (!mapInfo) return allStyle
 
-    const { styleName, prefix /** style值的前缀 */, hasSuffix /**后缀单位[px] */, valType } = mapInfo
+    const { styleName, prefix /** style值的前缀 */, hasUnit /**后缀单位[px] */, valType } = mapInfo
     const isCValue = valType === "classNameValue" // class="xx-value"
     const isCKey = valType === "classNameKey" // class="key-xx"
     const isCPercent = valType === "percent" // class="xx-value" => value/100
@@ -30,8 +30,8 @@ const getStyle = (classAry) => {
 
     let styleValue =
       (isCValue && classValue) || (isCKey && key) || (isCPercent && +classValue / 100) || (isCName && className)
-    isCValue && isNumberStr(styleValue) && (styleValue *= ratio) // 取xx-value数字值的进行缩放
-    hasSuffix && (styleValue += suffix) // 后缀单位
+    isCValue && isNumberStr(styleValue) && (styleValue *= valueRatio) // 取xx-value数字值的进行缩放
+    hasUnit && (styleValue += unit) // 后缀单位
 
     allStyle += `.${className} { ${styleName}: ${prefix ? prefix : ""}${styleValue}; }\n`
 
