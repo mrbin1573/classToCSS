12c07632c6635b1dfae9eb21098da580df16516f
hebin <1573406407@qq.com>
2022-07-18 15:54:44 +0800

    FEAT：优化apply获取class正则表达式

--- modules/getApplyClass.js
+++ modules/getApplyClass.js
@@ -5,7 +5,7 @@ const getApplyClass = async () => {
   let applyListAry = []
   try {
     const applyTxt = await getTextFromFilePath("\\" + APPLY_FILE_NAME)
-    const dividerReg = /\.(\w+)\s*\{\n*\s*@apply\s*(.*)\;/gim
+    const dividerReg = /\.(\S+)\s*\{\n*\s*@apply\s*(.*)\;/gim
     applyTxt.replace(dividerReg, ($0, $1, $2) => {
       let _classList = $2.split(/\s+/).map((item) => item.split(".")[1])
       applyListAry.push({ className: $1, classList: _classList })
